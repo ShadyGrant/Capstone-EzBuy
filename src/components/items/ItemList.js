3fcadebbae86b91bfd38576fe17ebcb2dd7823b5
Shady Grant <shadygrant@shadys-mbp.lan>
2020-05-02 10:47:54 -0500

    added item form

--- src/components/items/ItemList.js
+++ src/components/items/ItemList.js
@@ -2,17 +2,22 @@ import React, { useContext, useState } from "react"
 import { ItemContext } from "./ItemProvider"
 import { Item } from "./Item"
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap"
+import ItemForm from "./ItemForm"
 // import { BagContext } from "../bags/BagProvider"
+
 
 export default () => {
     const {items} = useContext(ItemContext)
     // const {bags}  = useContext(BagContext)
 
+    const [modal, setModal] = useState(false)
+    const toggle = () => setModal(!modal)
+
     return (
         <>
             <h2>My Items</h2>
 
-            {/* <button className="item_button" onClick={toggle}>Add New Item</button> */}
+            <button className="item_button" onClick={toggle}>Add New Item</button>
 
             <ul className="items">
             {
@@ -21,6 +26,14 @@ export default () => {
                     })
                 }
             </ul>
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle}>
+                    Add New Item
+                </ModalHeader>
+                <ModalBody>
+                    <ItemForm toggler={toggle} />
+                </ModalBody>
+            </Modal>
          </>
     )
 }
